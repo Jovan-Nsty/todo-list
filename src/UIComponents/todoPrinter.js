e44994a0757150d21fcce0271bc77aebf059c3cc
Jovan-Nsty <jova.zyo@gmail.com>
2024-02-28 17:56:12 +0100

    UIComponents/todoPrinter.js code refactor

--- src/UIComponents/todoPrinter.js
+++ src/UIComponents/todoPrinter.js
@@ -1,9 +1,11 @@
 export const printTodoArray = array => {
+    const getList = document.getElementById('project-todo-list');
+    getList.textContent = '';
+
     for (const todo of array) {
-        const getList = document.getElementById('project-todo-list');
         const makeListItem = document.createElement('li');
-        makeListItem.setAttribute('class','li-item');
         const makeAnchor = document.createElement('a');
+        makeListItem.setAttribute('class','li-item');
 
         makeAnchor.textContent = todo.getTodoTitle();
         makeListItem.appendChild(makeAnchor);
