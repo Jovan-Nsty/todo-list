33662970c5f4bfc51971a8e493d64a2d9f408bab
Jovan-Nsty <jova.zyo@gmail.com>
2024-02-28 19:57:19 +0100

    Code cleanup

--- src/app.js
+++ src/app.js
@@ -1,17 +1,12 @@
 import './style.css';
-import { addTodoToArray, todosArray } from './todo/todoManager.js';
+import { todosArray } from './todo/todoManager.js';
 import { printTodoArray } from './UIComponents/todoPrinter.js';
 import { createDefaultTodo } from './todo/defaultProject.js';
-import { createCustomTodo } from './todo/customProject.js';
 import './UIComponents/buttonFormTransform.js';
 import './UIComponents/retrieveFormValueAndUpdateDOM.js';
 
 // Creating a default 'personal' todo list on page load
 createDefaultTodo();
-
-// Creating custom todo projects on page load (beta)
-createCustomTodo('custom1');
-createCustomTodo('custom2');
 
 // Print all available todo projects
 printTodoArray(todosArray);
