0a5f1c04d0859567468021354ded8ae199661c0a
Jovan-Nsty <jova.zyo@gmail.com>
2024-03-28 15:13:38 +0100

    Update Date fix for each todo sub item / task

--- src/UIComponents/MainPanel/updateDueDate.js
+++ src/UIComponents/MainPanel/updateDueDate.js
@@ -1,33 +1,42 @@
 document.getElementById('todo-list-items').addEventListener('click', event => {
+    const eventTarget = event.target;
+
     if(event.target.classList.contains('today-btn')) {
         const date = new Date();
         const todayDate = date.toDateString();
 
-        printDateToDOM(todayDate);
+        printDateToDOM(todayDate, eventTarget);
     }
     if(event.target.classList.contains('tomorrow-btn')) {
         const tomorrowDate = new Date();
         tomorrowDate.setDate(tomorrowDate.getDate() + 1);
         const dateString = tomorrowDate.toDateString();
         
-        printDateToDOM(dateString);
+        printDateToDOM(dateString, eventTarget);
     }
 })
 
 document.getElementById('todo-list-items').addEventListener('input', event => {
-    const target = event.target;
-    if(target && target.classList && target.classList.contains('due-date-input')) {
-        const selectedDate = target.value;
+    const eventTarget = event.target;
+
+    if(eventTarget && eventTarget.classList && eventTarget.classList.contains('due-date-input')) {
+        const selectedDate = eventTarget.value;
         if(selectedDate) {
             const parsedDate = new Date(selectedDate);
             const formattedDate = parsedDate.toDateString();
             
-            printDateToDOM(formattedDate);
+            printDateToDOM(formattedDate, eventTarget);
         }
     }
 })
 
-function printDateToDOM(value) {
-    const listItem = document.querySelector('.todo-item-header > .text-content > .date-container > p');
-    listItem.textContent = value;
+function printDateToDOM(value, eventTarget) {
+    const closestListItem = eventTarget.closest('li');
+
+    if(closestListItem) {
+        const dateContainer = closestListItem.querySelector('.date-container > p');
+        if(dateContainer) {
+            dateContainer.textContent = value;
+        }
+    }
 }
