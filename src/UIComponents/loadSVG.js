de2d87922490b4fa9febe8f3dbf254edaa107ba7
Jovan-Nsty <jova.zyo@gmail.com>
2024-04-01 16:10:19 +0200

    Added SVG on Todo Project sub items expand button

--- src/UIComponents/loadSVG.js
+++ src/UIComponents/loadSVG.js
@@ -1,11 +1,13 @@
 import fileCheck from '../assets/file-check.svg';
 import delBtn from '../assets/delete.svg';
 import expandDownBtn from '../assets/expand-down.svg';
+import expandUpBtn from '../assets/expand-up.svg';
 
 export function loadSVG() {
     loadHeaderSVG();
 }
 
+// Function for loading the main SVG on <h1>
 function loadHeaderSVG() {
     const myIcon = new Image();
     myIcon.src = fileCheck;
@@ -16,6 +18,7 @@ function loadHeaderSVG() {
     return headerSlot;
 }
 
+// Function for loading <li> Todo Projecs delete button SVG
 export function loadDeleteBtnSVG(element) {
     const delIcon = new Image();
     delIcon.src = delBtn;
@@ -23,10 +26,24 @@ export function loadDeleteBtnSVG(element) {
     element.appendChild(delIcon);
 }
 
+// Function for loading the Todo Project task items button expand SVG
 export function loadExpandDownBtn(element) {
     const downIcon = new Image();
     downIcon.src = expandDownBtn;
     downIcon.classList.add('todo-list-item-expand-btn');
 
+    const upIcon = new Image();
+    upIcon.src = expandUpBtn;
+
     element.appendChild(downIcon);
+
+    element.addEventListener('click', () => {
+        if (element.classList.contains('down')) {
+            element.classList.remove('down');
+            downIcon.src = expandDownBtn; 
+        } else {
+            element.classList.add('down');
+            downIcon.src = expandUpBtn; 
+        }
+    })
 }
