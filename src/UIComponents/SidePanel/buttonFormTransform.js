1ca6d9bda4a5f724a3f7dc90ce74ad36c299ccd0
Jovan-Nsty <jova.zyo@gmail.com>
2024-04-02 11:33:50 +0200

    Fix issue of Add Todo button element not registering the click event

--- src/UIComponents/SidePanel/buttonFormTransform.js
+++ src/UIComponents/SidePanel/buttonFormTransform.js
@@ -1,5 +1,12 @@
+const span = document.querySelector('#todo-add-btn > span');
 const button = document.getElementById('todo-add-btn');
 const form = document.getElementById('todo-name');
+
+span.addEventListener('click', event => {
+    event.stopPropagation();
+    button.style.display = 'none';
+    form.style.display = 'flex';
+})
 
 button.addEventListener('click', () => {
     button.style.display = 'none';
